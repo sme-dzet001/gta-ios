cfdc4a4e1700fb6a5b8209800cb473b8266167a9
Margarita N. Bock <mbock@ksf.kiev.ua>
2020-11-05 12:28:51 +0200

    GTA-1.

---
+++ GTA/GTA/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GTA
+//
+//  Created by Margarita N. Bock on 05.11.2020.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
