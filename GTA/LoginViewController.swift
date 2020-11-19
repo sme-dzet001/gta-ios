d566607490af8149ff49a8eeb21a579850a62baf
Kostiantyn Dzetsiuk <kdzetsiuk@ksf.kiev.ua>
2020-11-19 16:49:38 +0200

    removed hardcoded login

--- GTA/LoginViewController.swift
+++ GTA/LoginViewController.swift
@@ -17,7 +17,6 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(tapGesture)
-        emailTextField.text = "dssdsdsd@sdds.dd"
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -27,7 +26,6 @@ class LoginViewController: UIViewController {
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-        onLoginButtonTap(sender: loginButton)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
