d49f6d55f08aef598d4b27980070fb810c73d0cf
Shmyhovskyi Ivan <bahbkawrx@gmail.com>
2020-12-02 19:29:29 +0200

    code structure changes

--- GTA/Communication/APIResponses/CommonResponses.swift
+++ GTA/Communication/APIResponses/CommonResponses.swift
@@ -18,3 +18,34 @@ struct ResponseMetaData: Codable {
         case userMessage = "user_message"
     }
 }
+
+// MARK: - Report Response
+
+struct SectionWidget: Codable {
+    var generationNumber: Int
+    var widgetId: String
+    
+    enum CodingKeys: String, CodingKey {
+        case generationNumber = "generation"
+        case widgetId = "widget_id"
+    }
+}
+
+struct ReportSection: Codable {
+    var title: String
+    var sectionId: String
+    var id: String
+    var widgets: [SectionWidget]?
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "title"
+        case sectionId = "section_id"
+        case id = "_id"
+        case widgets = "widgets"
+    }
+}
+
+struct ReportDataResponse: Codable {
+    var meta: ResponseMetaData
+    var data: [ReportSection]?
+}
--- GTA/Communication/APIResponses/GlobalNewsResponses.swift
+++ GTA/Communication/APIResponses/GlobalNewsResponses.swift
@@ -7,38 +7,39 @@
 
 import Foundation
 
-// MARK: - Report Response
-
-struct SectionWidget: Codable {
-    var generationNumber: Int
-    var widgetId: String
-    
-    enum CodingKeys: String, CodingKey {
-        case generationNumber = "generation"
-        case widgetId = "widget_id"
-    }
-}
-
-struct ReportSection: Codable {
-    var title: String
-    var sectionId: String
-    var id: String
-    var widgets: [SectionWidget]?
-    
-    enum CodingKeys: String, CodingKey {
-        case title = "title"
-        case sectionId = "section_id"
-        case id = "_id"
-        case widgets = "widgets"
-    }
-}
-
-struct ReportDataResponse: Codable {
-    var meta: ResponseMetaData
-    var data: [ReportSection]?
-}
-
 // MARK: - News Response
+
+struct GlobalNewsRow: Codable {
+    var values: [QuantumValue]?
+    
+    // maybe add here properties by index in array
+}
+
+struct GlobalNewsData: Codable {
+    var rows: [GlobalNewsRow]?
+}
+
+struct GlobalNewsResponse: Codable {
+    var meta: ResponseMetaData
+    var data: GlobalNewsData?
+}
+
+// MARK: - Special Alerts Response
+
+struct SpecialAlertRow: Codable {
+    var values: [QuantumValue]?
+    
+    // maybe add here properties by index in array
+}
+
+struct SpecialAlertsData: Codable {
+    var rows: [SpecialAlertRow]?
+}
+
+struct SpecialAlertsResponse: Codable {
+    var meta: ResponseMetaData
+    var data: SpecialAlertsData?
+}
 
 enum QuantumValue: Codable {
     
@@ -79,36 +80,4 @@ enum QuantumValue: Codable {
         case .string(let value): return value
         }
     }
-}
-
-struct GlobalNewsRow: Codable {
-    var values: [QuantumValue]?
-    
-    // maybe add here properties by index in array
-}
-
-struct GlobalNewsData: Codable {
-    var rows: [GlobalNewsRow]?
-}
-
-struct GlobalNewsResponse: Codable {
-    var meta: ResponseMetaData
-    var data: GlobalNewsData?
-}
-
-// MARK: - Special Alerts Response
-
-struct SpecialAlertRow: Codable {
-    var values: [QuantumValue]?
-    
-    // maybe add here properties by index in array
-}
-
-struct SpecialAlertsData: Codable {
-    var rows: [SpecialAlertRow]?
-}
-
-struct SpecialAlertsResponse: Codable {
-    var meta: ResponseMetaData
-    var data: SpecialAlertsData?
 }
