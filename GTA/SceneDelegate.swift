bd5878918cac886223ee69228ce7d2b6b60091b2
Kostiantyn Dzetsiuk <kdzetsiuk@ksf.kiev.ua>
2020-12-29 20:50:29 +0200

    GTA-441

--- GTA/SceneDelegate.swift
+++ GTA/SceneDelegate.swift
@@ -69,6 +69,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     lastActivityDate = aDate
                 }
                 if lastActivityDate.addingTimeInterval(1200) > Date(),  let _ = KeychainManager.getPin() {
+                    if let navController = self.window?.rootViewController as? UINavigationController, navController.rootViewController is UITabBarController {
+                        return
+                    }
                     let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
                     let mainViewController = storyBoard.instantiateViewController(withIdentifier: "TabBarController")
                     let navController = UINavigationController(rootViewController: mainViewController)
