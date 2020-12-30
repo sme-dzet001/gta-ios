8d542bb6193f4b3a8900815f6a02e46a5efa191a
Kostiantyn Dzetsiuk <kdzetsiuk@ksf.kiev.ua>
2020-12-30 15:37:45 +0200

    GTA-447

--- GTA/SceneDelegate.swift
+++ GTA/SceneDelegate.swift
@@ -11,13 +11,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    var appIsInTray: Bool = false
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-        showNeededScreen()
+        //showNeededScreen()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -40,6 +41,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        if !appIsInTray {
+            UserDefaults.standard.setValue(nil, forKey: "lastActivityDate")
+        }
+        appIsInTray = false
         showNeededScreen()
     }
 
@@ -48,6 +53,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
         UserDefaults.standard.setValue(Date(), forKey: "lastActivityDate")
+        appIsInTray = true
     }
     
     private func showNeededScreen() {
