1aefb016a3d8a7a1bfd0cc61c5670dff3eca9e97
Kostiantyn Dzetsiuk <kdzetsiuk@ksf.kiev.ua>
2021-01-04 16:04:59 +0200

    GTA-450

--- GTA/Login/AuthScreen/AuthViewController.swift
+++ GTA/Login/AuthScreen/AuthViewController.swift
@@ -128,9 +128,12 @@ class AuthViewController: UIViewController {
     func authenticateUser() {
         let context = LAContext()
         var error: NSError?
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+        if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
+            if context.biometryType == .none {
+                return
+            }
             let reason = "Authenticate with Biometrics"
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+            context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason) {
                 [weak self] success, authenticationError in
                 self?.checkAuthentification(isSuccess: success, error: authenticationError as NSError?)
             }
