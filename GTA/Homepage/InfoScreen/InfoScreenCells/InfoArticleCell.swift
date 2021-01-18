a6d6c3c795eab16505fa1d9a4288466fa3a0859c
Kostiantyn Dzetsiuk <kdzetsiuk@ksf.kiev.ua>
2021-01-18 13:04:33 +0200

    GTA-665

--- GTA/Homepage/InfoScreen/InfoScreenCells/InfoArticleCell.swift
+++ GTA/Homepage/InfoScreen/InfoScreenCells/InfoArticleCell.swift
@@ -24,8 +24,11 @@ class InfoArticleCell: UITableViewCell {
         let matches = detector.matches(in: text, options: [], range: NSRange(location: 0, length: text.utf16.count))
         for match in matches {
             if gesture.didTapAttributedTextInLabel(label: infoLabel, inRange: match.range) {
-                guard let range = Range(match.range, in: text), let url = URL(string: "\(text[range])")  else { continue }
-                delegate?.openUrl(url)
+                guard let range = Range(match.range, in: text) else { continue }
+                let urlString = "\(text[range])".hasPrefix("http") ? "\(text[range])" : "https://\(text[range])"
+                if let url = URL(string: urlString) {
+                    delegate?.openUrl(url)
+                }
             }
         }
     }
