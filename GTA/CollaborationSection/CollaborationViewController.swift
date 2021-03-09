e4619472aea89b95777d9ffd7cf15876629b72c7
Kostiantyn Dzetsiuk <kdzetsiuk@ksf.kiev.ua>
2021-03-09 14:40:52 +0200

    save GTA-1021

---
+++ GTA/CollaborationSection/CollaborationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CollaborationViewController.swift
+//  GTA
+//
+//  Created by Kostiantyn Dzetsiuk on 09.03.2021.
+//
+
+import UIKit
+
+class CollaborationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
