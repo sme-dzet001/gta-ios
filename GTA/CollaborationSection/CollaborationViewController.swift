91ebf1324f7024323f9177c64dd1b4973aafe151
Serhii Kovtunenko <skovtunenko@ksf.kiev.ua>
2021-04-02 21:53:44 +0300

    GTA-1234 Icons

--- GTA/CollaborationSection/CollaborationViewController.swift
+++ GTA/CollaborationSection/CollaborationViewController.swift
@@ -90,9 +90,9 @@ class CollaborationViewController: UIViewController {
     
     private func setUpHardCodeData() {
         dataSource.append(CollaborationCellData(cellTitle: dataProvider.collaborationDetails?.description, updatesNumber: nil))
-        dataSource.append(CollaborationCellData(imageName: nil, cellTitle: "Office 365 Applications", cellSubtitle: "Create, Collaborate & Connect", updatesNumber: nil, imageStatus: .loading))
-        dataSource.append(CollaborationCellData(imageName: "quick_help_icon", cellTitle: "Tips & Tricks", cellSubtitle: "Get the most from the app", updatesNumber: nil))
-        dataSource.append(CollaborationCellData(imageName: "contacts_icon", cellTitle: "Team Contacts", cellSubtitle: "Key Contacts and Member Profiles", updatesNumber: nil))
+        dataSource.append(CollaborationCellData(imageName: "applications_icon", cellTitle: "Office 365 Applications", cellSubtitle: "Create, Collaborate & Connect", updatesNumber: nil, imageStatus: .loading))
+        dataSource.append(CollaborationCellData(imageName: "tips_n_tricks_icon", cellTitle: "Tips & Tricks", cellSubtitle: "Get the most from the app", updatesNumber: nil))
+        dataSource.append(CollaborationCellData(imageName: "team_contacts_icon", cellTitle: "Team Contacts", cellSubtitle: "Key Contacts and Member Profiles", updatesNumber: nil))
     }
     
     private func showContactsScreen() {
@@ -147,12 +147,6 @@ extension CollaborationViewController: UITableViewDelegate, UITableViewDataSourc
             cell.descriptionLabel.text = dataProvider.collaborationDetails?.description//createAttributedString(for: dataProvider.collaborationDetails?.description)
             return cell
         }
-        if indexPath.row == 1, let cell = tableView.dequeueReusableCell(withIdentifier: "Office365AppCell", for: indexPath) as? Office365AppCell {
-            cell.appTitleLabel.text = dataSource[indexPath.row].cellTitle
-            cell.descriptionLabel.text = dataSource[indexPath.row].cellSubtitle
-            cell.setImage(with: dataSource[indexPath.row].imageData, status: dataSource[indexPath.row].imageStatus)
-            return cell
-        }
         if let cell = tableView.dequeueReusableCell(withIdentifier: "HelpDeskCell", for: indexPath) as? HelpDeskCell {
             let cellData = dataSource[indexPath.row]
             cell.setUpCell(with: cellData, isActive: true, isNeedCornerRadius: true)
@@ -180,12 +174,6 @@ extension CollaborationViewController: AppSuiteIconDelegate {
         DispatchQueue.main.async {
             if let _ = data {
                 self.headerTitleView.headerImageView.image = UIImage(data: data!)
-            }
-            guard self.dataSource.count > 1 else { return }
-            if let cell = self.tableView.cellForRow(at: IndexPath(row: 1, section: 0)) as? Office365AppCell {
-                self.dataSource[1].imageData = data
-                self.dataSource[1].imageStatus = status
-                cell.setImage(with: data, status: status)
             }
         }
     }
