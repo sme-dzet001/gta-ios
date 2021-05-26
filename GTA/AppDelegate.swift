749dbe5b2955445f2ab8caa0f3dd4dd5fde26cf7
Kostiantyn Dzetsiuk <kdzetsiuk@ksf.kiev.ua>
2021-05-26 20:05:11 +0300

    small changes

--- GTA/AppDelegate.swift
+++ GTA/AppDelegate.swift
@@ -12,7 +12,7 @@ import UserNotifications
 import PanModal
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -36,7 +36,88 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         return .portrait
     }
     
-    func registerForPushNotifications() {
+    private func topViewController(controller: UIViewController?) -> UIViewController? {
+        if let navigationController = controller as? UINavigationController {
+            return topViewController(controller: navigationController.visibleViewController)
+        }
+        if let tabController = controller as? UITabBarController {
+            if let selected = tabController.selectedViewController {
+                return topViewController(controller: selected)
+            }
+        }
+        if let presented = controller?.presentedViewController {
+            return topViewController(controller: presented)
+        }
+        return controller
+    }
+    
+    func dismissPanModalIfPresented(completion: @escaping (() -> Void)) {
+        if let panModal = getTopViewController() as? PanModalPresentable {
+            (panModal as? UIViewController)?.dismiss(animated: true, completion: completion)
+        } else {
+            completion()
+        }
+    }
+    
+    private func getTopViewController() -> UIViewController? {
+        let keyWindow = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+        var topVC: UIViewController?
+        if var VC = keyWindow?.rootViewController {
+            while let presentedViewController = VC.presentedViewController {
+                VC = presentedViewController
+            }
+            topVC = topViewController(controller: VC)
+        }
+        return topVC
+    }
+    // MARK: - UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    // MARK: - Core Data context
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "GTA")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    lazy var databaseContext: NSManagedObjectContext = {
+        return self.persistentContainer.viewContext
+    }()
+}
+
+// MARK: - UNUserNotificationCenterDelegate (Push Notification)
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound, .badge])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        if let payload = userInfo["payload"] as? String, let payloadData = Data(base64Encoded: payload), let payloadDict = try? JSONSerialization.jsonObject(with: payloadData, options: .mutableContainers) as? [String : AnyObject], let pushType = payloadDict["push_type"] as? String, pushType == "Emergency Outage Global" {
+            NotificationCenter.default.post(name: Notification.Name(NotificationsNames.emergencyOutageNotificationReceived), object: nil)
+        }
+    }
+}
+
+extension AppDelegate {
+    
+    private func registerForPushNotifications() {
         UNUserNotificationCenter.current()
             .requestAuthorization(options: [.alert, .badge, .sound]) { [weak self] granted, _ in
                 guard granted else { return }
@@ -45,7 +126,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         UNUserNotificationCenter.current().delegate = self
     }
     
-    func getNotificationSettings() {
+    private func getNotificationSettings() {
         UNUserNotificationCenter.current().getNotificationSettings { settings in
             guard settings.authorizationStatus == .authorized else { return }
             DispatchQueue.main.async {
@@ -85,91 +166,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     ) {
         //TODO: process notification
     }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.alert, .sound, .badge])
-    }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        let userInfo = response.notification.request.content.userInfo
-        if let payload = userInfo["payload"] as? String, let payloadData = Data(base64Encoded: payload), let payloadDict = try? JSONSerialization.jsonObject(with: payloadData, options: .mutableContainers) as? [String : AnyObject], let pushType = payloadDict["push_type"] as? String, pushType == "Emergency Outage Global" {
-            NotificationCenter.default.post(name: Notification.Name(NotificationsNames.emergencyOutageNotificationReceived), object: nil)
-        }
-    }
-    
-//    private func topViewControllerWithRootViewController(rootViewController: UIViewController!) -> UIViewController? {
-//        if (rootViewController == nil) { return nil }
-//        if (rootViewController.isKind(of: UITabBarController.self)) {
-//          return topViewControllerWithRootViewController(rootViewController: (rootViewController as! UITabBarController).selectedViewController)
-//        } else if (rootViewController.isKind(of: UINavigationController.self)) {
-//          return topViewControllerWithRootViewController(rootViewController: (rootViewController as! UINavigationController).visibleViewController)
-//        } else if (rootViewController.presentedViewController != nil) {
-//          return topViewControllerWithRootViewController(rootViewController: rootViewController.presentedViewController)
-//        }
-//        return rootViewController
-//      }
-    
-    private func topViewController(controller: UIViewController?) -> UIViewController? {
-        if let navigationController = controller as? UINavigationController {
-            return topViewController(controller: navigationController.visibleViewController)
-        }
-        if let tabController = controller as? UITabBarController {
-            if let selected = tabController.selectedViewController {
-                return topViewController(controller: selected)
-            }
-        }
-        if let presented = controller?.presentedViewController {
-            return topViewController(controller: presented)
-        }
-        return controller
-    }
-    
-    func dismissPanModalIfPresented(completion: @escaping (() -> Void)) {
-        if let panModal = getTopViewController() as? PanModalPresentable {
-            (panModal as? UIViewController)?.dismiss(animated: true, completion: completion)
-        } else {
-            completion()
-        }
-    }
-    
-    private func getTopViewController() -> UIViewController? {
-        let keyWindow = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
-        var topVC: UIViewController?
-        if var VC = keyWindow?.rootViewController {
-            while let presentedViewController = VC.presentedViewController {
-                VC = presentedViewController
-            }
-            topVC = topViewController(controller: VC)
-        }
-        return topVC
-    }
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-    
-    // MARK: - Core Data context
-    lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "GTA")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-    
-    lazy var databaseContext: NSManagedObjectContext = {
-        return self.persistentContainer.viewContext
-    }()
 }
-
