96eb939c8b5adda7e05e2e95a5d3b22dd552340b
Fublvckmagik <artem_khreshchenyuk@icloud.com>
2021-07-08 12:37:18 +0300

    GTA-1815 Grid was added

--- GTA/CollaborationSection/UsageMetricsScreen/ChartControllers/TeamChatUsersViewController.swift
+++ GTA/CollaborationSection/UsageMetricsScreen/ChartControllers/TeamChatUsersViewController.swift
@@ -14,26 +14,46 @@ class TeamChatUsersViewController: UIViewController {
     @IBOutlet weak var chartView: HorizontalBarChartView!
     
     var dataProvider: UsageMetricsDataProvider?
+    var gridView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         updateChartData()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        addGridView()
+    }
+    
     func updateChartData() {
         guard let activeUsersData = dataProvider?.teamsChatUserData.sorted(by: {$0.percent ?? 0 < $1.percent ?? 0}), !activeUsersData.isEmpty else { return }
         let chartValues = activeUsersData.enumerated().map { (index, dataEntry) -> BarChartDataEntry in
             return BarChartDataEntry(x: Double(index), y: dataEntry.percent ?? 0)
         }
-
-        setChartView()
-        setXAxis(activeUsersData)
-        setLeftAxis(activeUsersData)
-        setRightAxix()
-
-
-        let dataSet = BarChartDataSet(entries: chartValues, label: nil)
+        
+        setChartView(activeUsersData)
+        setData(chartValues)
+    }
+    
+    func addGridView() {
+        gridView = setGridView()
+        chartView.addSubview(gridView)
+        gridView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            gridView.leadingAnchor.constraint(equalTo: chartView.leadingAnchor, constant: 60),
+            gridView.trailingAnchor.constraint(equalTo: chartView.trailingAnchor, constant: -32),
+            gridView.topAnchor.constraint(equalTo: chartView.topAnchor, constant: 10),
+            gridView.bottomAnchor.constraint(equalTo: chartView.bottomAnchor, constant: -10)
+        ])
+        
+        setHorizontalLines(linesCount: 9, lineHeight: 1)
+    }
+    
+    private func setData(_ values: [BarChartDataEntry]) {
+        let dataSet = BarChartDataSet(entries: values, label: nil)
         dataSet.setColor(UIColor(hex: 0x428DF7))
         dataSet.valueFont = .systemFont(ofSize: 14)
         
@@ -42,49 +62,10 @@ class TeamChatUsersViewController: UIViewController {
         data.barWidth = 0.59
         data.setValueFormatter(DefaultValueFormatter(formatter: procentFormatter()))
         chartView.data = data
-        
-        let gridView = gridView()
-        chartView.addSubview(gridView)
-        gridView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            gridView.leadingAnchor.constraint(equalTo: chartView.leadingAnchor, constant: 60),
-            gridView.trailingAnchor.constraint(equalTo: chartView.trailingAnchor, constant: -32),
-            gridView.topAnchor.constraint(equalTo: chartView.topAnchor, constant: 8),
-            gridView.bottomAnchor.constraint(equalTo: chartView.bottomAnchor, constant: -8)
-        ])
     }
     
-    private func setXAxis(_ userData: [TeamsChatUserDataEntry]) {
-        let countries = userData.map{data -> String in
-            return data.countryCode ?? ""
-        }
-        
-        let xAxis = chartView.xAxis
-        xAxis.labelPosition = .bottom
-        xAxis.drawGridLinesEnabled = false
-        xAxis.drawAxisLineEnabled = false
-        xAxis.labelCount = countries.count
-        xAxis.valueFormatter = IndexAxisValueFormatter(values: countries)
-        xAxis.labelFont = .systemFont(ofSize: 14)
-        xAxis.labelTextColor = .lightGray
-    }
-    
-    private func setLeftAxis(_ userData: [TeamsChatUserDataEntry]) {
-        let leftAxis = chartView.leftAxis
-        let maxValueBarMultiplier: Double = 1.4
-        let maxValue = userData.max(by: {$0.percent ?? 0 < $1.percent ?? 0})?.percent ?? 0
-        leftAxis.axisMaximum = maxValueBarMultiplier * maxValue
-        leftAxis.enabled = false
-        leftAxis.drawGridLinesEnabled = false
-    }
-    
-    private func setRightAxix() {
-        let righAxis = chartView.rightAxis
-        righAxis.enabled = false
-        righAxis.drawGridLinesEnabled = false
-    }
-    
-    private func setChartView() {
+    private func setChartView(_ activeUsers: [TeamsChatUserDataEntry]) {
+        // Chart
         chartView.isUserInteractionEnabled = false
         chartView.chartDescription?.enabled = false
         chartView.chartDescription?.text = nil
@@ -97,15 +78,30 @@ class TeamChatUsersViewController: UIViewController {
         chartView.rightAxis.enabled = false
         chartView.drawValueAboveBarEnabled = true
         chartView.maxVisibleCount = 60
-    }
-    
-    private func gridView() -> UIView {
-        let gv = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        gv.borderWidth = 1
-        gv.borderColor = .lightGray
-        gv.backgroundColor = .clear
         
-        return gv
+        // XAxis
+        let xAxis = chartView.xAxis
+        xAxis.labelPosition = .bottom
+        xAxis.drawGridLinesEnabled = false
+        xAxis.drawAxisLineEnabled = false
+        xAxis.labelCount = activeUsers.count
+        xAxis.valueFormatter = IndexAxisValueFormatter(values: activeUsers.map{data -> String in return data.countryCode ?? ""
+        })
+        xAxis.labelFont = .systemFont(ofSize: 14)
+        xAxis.labelTextColor = .lightGray
+        
+        // LeftAxis
+        let leftAxis = chartView.leftAxis
+        let maxValueBarMultiplier: Double = 1.4
+        let maxValue = activeUsers.max(by: {$0.percent ?? 0 < $1.percent ?? 0})?.percent ?? 0
+        leftAxis.axisMaximum = maxValueBarMultiplier * maxValue
+        leftAxis.enabled = false
+        leftAxis.drawGridLinesEnabled = false
+        
+        // RightAxis
+        let righAxis = chartView.rightAxis
+        righAxis.enabled = false
+        righAxis.drawGridLinesEnabled = false
     }
 }
 
@@ -118,5 +114,29 @@ extension TeamChatUsersViewController {
         formatter.percentSymbol = "%"
         
         return formatter
+    }
+}
+// Grid functions
+extension TeamChatUsersViewController {
+    private func setGridView() -> UIView {
+        let gv = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        gv.borderWidth = 1
+        gv.borderColor = .lightGray
+        gv.backgroundColor = .clear
+        
+        return gv
+    }
+    
+    private func setHorizontalLines(linesCount: Int, lineHeight: CGFloat = 1) {
+        guard linesCount > 1 else { return }
+        let containerHeight = gridView.frame.height
+        let containerWidth = gridView.frame.width
+        let linesSpacing = (containerHeight - CGFloat(linesCount + 1) * lineHeight) / (CGFloat(linesCount) + 1)
+        
+        for i in 1..<linesCount + 1 {
+            let lineView = UIView(frame: CGRect(x: 0, y: (linesSpacing + lineHeight) * CGFloat(i), width: containerWidth, height: lineHeight))
+            lineView.backgroundColor = .lightGray
+            gridView.addSubview(lineView)
+        }
     }
 }
