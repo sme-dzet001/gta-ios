2be33b0b53b9213a9ede748e2275d35bddc0aa05
Fublvckmagik <artem_khreshchenyuk@icloud.com>
2021-10-11 18:45:40 +0300

    GTA-2609

--- GTA/CollaborationSection/UsageMetricsScreen/ChartCells/TeamsByFunctionsTableViewCell.swift
+++ GTA/CollaborationSection/UsageMetricsScreen/ChartCells/TeamsByFunctionsTableViewCell.swift
@@ -92,6 +92,7 @@ class TeamsByFunctionsTableViewCell: UITableViewCell {
             updateLabels()
             updateChartData()
             updateSelectorBtns()
+            defaultScrollPosition()
         }
     }
 
